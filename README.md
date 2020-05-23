18c4e6521e22466e22153f71c459d0d363d1f469
Marc Juul <marc@juul.io>
2020-05-23 02:06:31 -0700

    What previous commit claimed to do

--- README.md
+++ README.md
@@ -6,13 +6,43 @@ sudo apt install build-essential libudev-dev printer-driver-dymo
 npm install 
 ```
 
+# Running
+
 `./bin/cmd.js`: Run this at root. Detects devices being plugged in and installs them.
 
 `./bin/list.js`: Run this as the renegade user. Lists devices that are both installed and plugged in.
 
-# ToDo
+# Options
 
-* Integrate this into renegade-labdevice daemon
+```
+--debug: Enable debug output
+--fake: Don't actually install anything
+--use-usb-uri: Install printers using 'usb:' URIs intead of 'file:' URIs
+--no-check-cups: Don't check CUPS config for FileDevice line
+--uninstall-all: Uninstall all CUPS printers installed by this program
+```
+
+Some DYMO printers have an issue where CUPS detects the same wrong serial number no matter which printer is plugged in. For this reason we default to using `file:` URIs for DYMO printers, but if you want to use `usb:` URIs then you can try the `--use-usb-uri` argument. 
+
+Warning: `--uninstall-all` uses `/dev/printers/` to determine which printers were installed by this program. It assumes the last part of each file name before the `.rules` is the printer's serial number and the first part is the printer's vendor. If you have anything else in this directory that wasn't installed by this program but follows this pattern then it could be removed when running with this argument.
+
+# Setup
+
+TODO installing CUPS and required commands.
+
+CUPS has trouble handling some types of printers. No matter how many are plugged in, CUPS just see them all as one printer and lists the wrong serial number. This has been seen to happen for some people with the DYMO LabelWriter 450. If you know you don't have a problem and really want to use `usb:` URIs for the installed DYMO printers then use the `--use-usb-uri` argument.
+
+Otherwise edit `/etc/cups/cups-files.conf` to enable File device URIs by uncommenting and changing the `FileDevice` line:
+
+```
+FileDevice Yes
+```
+
+Then restart cups:
+
+```
+sudo /etc/init.d/cups restart
+```
 
 # Useful CUPS commands
 
@@ -31,7 +61,7 @@ lpoptions -d DYMO-LabelWriter-450-Turbo -l
 Print with custom paper size:
 
 ```
-lpr -P DYMO-LabelWriter-450-Turbo -o media=Custom.15x39mm example.png
+lpr -P DYMO-LabelWriter-450-Turbo -o media=Custom.20x39mm examples/example.png
 ```
 
 To list all currently connected USB printers (not limited to installed printers):
@@ -61,7 +91,7 @@ sudo lpadmin -x LabelWriter-450-turbo
 
 # Copyright and License
 
-* Copyright 2020 renegade.bio
 * Copyright 2017-2018 BioBricks Foundation
+* Copyright 2020 renegade.bio
 
-License: Dual-licensed under GPLv3 and AGPLv3. You may use this software under the terms of either or both as you prefer.
+License: GPLv3
--- bin/cmd.js
+++ bin/cmd.js
@@ -2,28 +2,93 @@
 
 var fs = require('fs');
 var path = require('path');
-var exec = require('child_process').exec;
+var doExec = require('child_process').exec;
 var async = require('async');
 var usbDetect = require('usb-detection');
+var minimist = require('minimist');
 
-var udevDir = "/etc/udev/rules.d";
+var UDEV_DIR = "/etc/udev/rules.d";
 
-var vendorIDs = {
-  brother: "04f9",
-  dymo: "?"
-};
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    d: 'debug' // enable debug mode
+    
+  },
+  boolean: [
+    'debug',
+    'use-usb-uri', // install printers using file: URIs
+    'uninstall-all'
+  ]
+});
+
+var CUPS_FILES_CONFIG_PATH = '/etc/cups/cups-files.conf';
+var DEV_SUBDIR = 'printers';
+var DEV_DIR = path.join('/dev', DEV_SUBDIR);
 
 var supportedDevices = {
-  brother: {
+  brother: [
 
-  },
-  dymo: {
-
-  }
+  ],
+  dymo: [
+    0x0922
+  ]
 }
 
-function udevCheckLine(line, vendor, serial) {
-  if(!line.match('ATTRS{idVendor}=="'+vendorIDs[vendor]+'"')) {
+function debug() {
+  if(!argv.debug) return;
+  var args = Array.prototype.splice.call(arguments, 0);
+  args = ['[debug]'].concat(args);
+
+  console.log.apply(null, args);
+}
+
+function exec(cmd, opts, cb) {
+  if(typeof opts === 'function') {
+    cb = opts;
+    opts = {};
+  }
+  if(argv.fake) {
+    debug("--fake enabled so not actually running command:", cmd);
+  } else {
+    debug("Running command:", cmd);
+  }
+  doExec(cmd, opts, cb);
+}
+
+function uninstallAll() {
+  console.log("TODO not fully implemented");
+  
+  fs.readdir(UDEV_DIR, function(err, files) {
+    if(err) return cb(err);
+    async.eachSeries(files, function(file, next) {
+
+      next();
+    });
+  });
+}
+
+function vendorIDToHex(vendorID) {
+  if(typeof vendorID === 'string') {
+    vendorID = parseInt(vendorID);
+  }
+  var str = vendorID.toString(16);
+  
+  var zeroes = 4 - str.length;
+  var i;
+  for(i=0; i < zeroes; i++) {
+    str = '0' + str;
+  }
+  return str;
+}
+
+function udevCheckLine(line, vendorID, serial) {
+  if(!line || !line.trim()) return false;
+
+  var hexVendorID = vendorIDToHex(vendorID);
+  
+  debug("Checking for vendorID", hexVendorID, "and serial", serial, "on line:");
+  debug("  " + line);
+  if(!line.match('ATTRS{idVendor}=="'+hexVendorID+'"')) {
     return false;
   }
 
@@ -31,23 +96,27 @@ function udevCheckLine(line, vendor, serial) {
     return false;
   }
 
+  debug("  FOUND!");
   return true;
 }
 
-function isBrotherInstalled(vendor, serial, cb) {
-  fs.readdir(udevDir, function(err, files) {
+function isUdevInstalled(vendor, vendorID, serial, cb) {
+  var rulesRegexp = new RegExp("^"+vendor+"-.+\.rules$");
+  
+  fs.readdir(UDEV_DIR, function(err, files) {
     if(err) return cb(err);
     async.eachSeries(files, function(file, next) {
-      if(!file.match(/^bionet-.+\.rules$/)) return next();
+      if(!file.match(rulesRegexp)) return next();
       
-      fs.readFile(path.join(udevDir, file), {encoding: 'utf8'}, function(err, data) {
+      fs.readFile(path.join(UDEV_DIR, file), {encoding: 'utf8'}, function(err, data) {
         if(err) return cb(err);
         
         var lines = data.split(/\r?\n/);
         var i, m;
         
         for(i=0; i < lines.length; i++) {
-          if(udevCheckLine(lines[i], vendor, serial)) {
+
+          if(udevCheckLine(lines[i], vendorID, serial)) {
             return cb(null, true);
           }
         }
@@ -58,24 +127,30 @@ function isBrotherInstalled(vendor, serial, cb) {
   });
 }
 
-
-// ATTRS{idVendor}=="04f9", ATTRS{serial}=="B3Z595204", SYMLINK+="bionet/brother-B3Z595204"
-
-function installBrother(vendor, serial, cb) {
-  isBrotherInstalled(vendor, serial, function(err, isInstalled) {
-    if(err) return cb(err);
-    if(isInstalled) return cb();
-  });
+function installUdevRule(vendor, vendorID, serial, cb) {
 
   var elements = [];
 
-  elements.push('ATTRS{idVendor}=="'+vendorIDs[vendor]+'"');
-  elements.push('ATTRS{serial}=="'+serial+'"');
-  elements.push('SYMLINK+="bionet/brother-'+serial+'"');
-
-  var line = "\n"+elements.join(", ")+"\n";
+  var vendorIDHex = vendorIDToHex(vendorID);
   
-  fs.writeFile(path.join(udevDir, 'bionet-brother-'+serial+'.rules'), line, {encoding: 'utf8'}, function(err) {
+  elements.push('SUBSYSTEM=="usbmisc"');
+  elements.push('ATTRS{idVendor}=="'+vendorIDHex+'"');
+  elements.push('ATTRS{serial}=="'+serial+'"');
+  elements.push('SYMLINK+="'+getUdevPath(vendor, serial)+'"');
+
+  var line = elements.join(", ")+"\n";
+
+  var fileToWrite = path.join(UDEV_DIR, vendor+'-'+serial+'.rules');
+
+  debug("Writing udev rule to", fileToWrite);
+  debug("  rule:", line);
+
+  if(argv.fake) {
+    debug("  --fake enabled so not actually writing");
+    return cb(null, true);
+  }
+  
+  fs.writeFile(fileToWrite, "\n"+line, {encoding: 'utf8'}, function(err) {
     if(err) return cb(err);
 
     // let udev know we updated things
@@ -87,36 +162,104 @@ function installBrother(vendor, serial, cb) {
   });
 }
 
-function isDymoInstalled(uri, cb) {
+function installBrother(vendor, vendorID, serial, cb) {
+  isUdevInstalled(vendor, vendorID, serial, function(err, isInstalled) {
+    if(err) return cb(err);
+    if(isInstalled) return cb();
+  });
 
-  // list all installed printers
-  exec("lpstat -v", function(err, stdout, stderr) {
-    if(err) return cb(err + "\n" + stderr);  
+  installUdevRule(vendor, vendorID, serial, cb);
+}
+
+function uriType(uri) {
+  var m = uri.match(/^(file|usb):/);
+  if(!m) throw new Error("Unknown URI type: " + uri);
+
+  return m[1];
+}
+
+function isCupsInstalled(uri, vendor, vendorID, serial, cb) {
+
+  if(uriType(uri) === 'file') {
     
-    var lines = stdout.split(/\r?\n/);
-    var i, m;
+    isUdevInstalled(vendor, vendorID, serial, cb);
+    return;
+    
+  } else {
+    
+    // list all installed printers
+    exec("lpstat -v", function(err, stdout, stderr) {
+      if(err) return cb(err + "\n" + stderr);  
+      
+      var lines = stdout.split(/\r?\n/);
+      var i, m;
+      for(i=0; i < lines.length; i++) {
+        if(lines[i].indexOf(uri) >= 0) {
+          return cb(null, true);
+        }
+      }
+      cb(null, false);
+    });
+    return;
+  }
+}
+
+function checkFileDevice(cb) {
+  cb = cb || function(){};
+  if(argv['use-usb-uri']) {
+    return;
+  }
+
+  debug("Checking if the 'FileDevice' option is enabled in:", CUPS_FILES_CONFIG_PATH);
+  
+  fs.readFile(CUPS_FILES_CONFIG_PATH, {encoding: 'utf8'}, function(err, data) {
+    if(err) {
+      console.log("WARNING: Could not read", CUPS_FILES_CONFIG_PATH);
+      console.log("  which makes it impossible to determine if the 'FileDevice'");
+      console.log("  option is enabled for CUPS. If you are using CUPS printers");
+      console.log("  then please ensure that this option is enabled");
+      return cb();
+    }
+
+    var lines = data.split(/\r?\n/);
+    var i, line;
     for(i=0; i < lines.length; i++) {
-      if(lines[i].indexOf(uri) >= 0) {
+      line = lines[i];
+      if(line.match(/^\s*#/)) continue;
+      if(line.match(/^\s*FileDevice\s*Yes/)) {
+        debug("FileDevice option is enabled!");
         return cb(null, true);
       }
     }
-    cb(null, false);
+
+    console.log("WARNING: It looks like the 'FileDevice' option is not enabled in:", CUPS_FILES_CONFIG_PATH);
+    console.log("  If you are using any CUPS printers then please see");
+    console.log("  the README.md file for how to enable this option.");
+    
+    return cb(null, false);
   });
 }
 
 function cupsFindDriver(model, cb) {
   var modelReg = model.replace(/\s+/g, "\\s+");
-  var r = new RegExp("^([^\s]+)\.ppd.+"+modelReg+"\s*$", 'i');
+  var r = new RegExp("^([^\\s]+)\.ppd.+"+modelReg+"\\s*$", 'i');
 
+  var cmd = "lpinfo -m";
+
+  debug("Listing all installable printer drivers using:", cmd);
+  debug("while looking for a match to:", modelReg);
+  debug("(please stand by, this can take a while)");
+  
   // list all installed drivers
-  exec("lpinfo -m", {maxBuffer: 1024 * 2000}, function(err, stdout, stderr) {
+  exec(cmd, {maxBuffer: 1024 * 2000}, function(err, stdout, stderr) {
     if(err) return cb(err + "\n" + stderr);
     var lines = stdout.split(/\r?\n/);
     var i, m, line;
     for(i=0; i < lines.length; i++) {
       line = lines[i];
-      console.log("LINE:", line);
+
       if(m = line.match(r)) {
+        debug("  Found:", m[0]);
         return cb(null, m[1]); // m[1] is the driver name
       }
     }
@@ -124,61 +267,135 @@ function cupsFindDriver(model, cb) {
   });
 }
 
-function installDymo(uri, model, serial, cb) {
-  if(uri.match('"') || model.match('"') || serial.match('"')) {
-    return cb(new Error('uri, model or serial of usb device contained a " (double-quotes) character. This would result in badness and could be an attempt at accomplishing something malicious.'));
+function activateCupsPrinter(printerName, cb) {
+
+  var cmd = 'sudo cupsenable "'+printerName+'"';
+    
+  exec(cmd, function(err, stdout, stderr) {
+    if(err) cb(err);
+    
+    cmd = 'sudo cupsaccept "'+printerName+'"';
+    
+    exec(cmd, cb);
+  });
+}
+
+function installCups(vendor, vendorID, model, serial, cb) {
+  if(vendor.match('"') || model.match('"') || serial.match('"')) {
+    return cb(new Error('vendor, model or serial of usb device contained a " (double-quotes) character. This would result in badness and could be an attempt at accomplishing something malicious.'));
   }
 
-  isDymoInstalled(uri, function(err, isInstalled) {
+  getURI(vendor, serial, function(err, uri) {
     if(err) return cb(err);
-    if(isInstalled) return cb();
-    
-    cupsFindDriver(model, function(err, driverName) {
-      if(err) return cb(err);
-      if(!driverName) return cb(new Error("No driver found for printer:", model, uri));
-      var printerName = model.replace(/\s+/g, '-') + '-' + serial;
 
-      exec('lpadmin -p "'+printerName+'" -E -v "'+uri+'" -m "'+driverName+'.ppd"', function(err, stdout, stderr) {
-        if(err) return cb(err + "\n" + stderr);
-        cb(null, true);
-      }); 
+    debug("Device URI:", uri);
+    
+    isCupsInstalled(uri, vendor, vendorID, serial, function(err, isInstalled) {
+      if(err) return cb(err);
+      
+      debug("Is device already installed:", (isInstalled) ? "Yes" : "No");
+      
+      if(isInstalled) return cb();
+      
+      cupsFindDriver(model, function(err, driverName) {
+        if(err) return cb(err);
+        if(!driverName) return cb(new Error("No driver found for printer:", model, uri));
+
+        installUdevRule(vendor, vendorID, serial, function(err) {
+        
+          var printerName = model.replace(/\s+/g, '-') + '-' + serial;
+
+          var cmd = 'lpadmin -p "'+printerName+'" -E -v "'+uri+'" -m "'+driverName+'.ppd"';
+          
+          exec(cmd, function(err, stdout, stderr) {
+            if(err) return cb(err + "\n" + stderr);
+
+            activateCupsPrinter(printerName, function(err) {
+              cb(null, true);
+            });
+          });
+        });
+      });
     });
   });
 }
 
+function getUdevPath(vendor, serial) {
+  return path.join(DEV_SUBDIR, vendor+'-'+serial);
+}
 
-// vendor is "brother" or "dymo"
-function installPrinter(vendor, model, serial, cb) {
-  vendor = vendor.toLowerCase();
-  model = model.replace(/_/g, ' ');
+function getFileURI(vendor, serial, cb) {
+  var uri = 'file:'+path.join('/dev', getUdevPath(vendor, serial));
+  
+  cb(null, uri);
+}
 
+function getUsbURI(vendor, serial, cb) {
   var r = new RegExp("usb://"+vendor+".+serial="+serial, 'i')
-  console.log("VENDER:", vendor, "||", model, "||", serial);
+
   // list all connected printers
   exec("lpinfo -v", function(err, stdout, stderr) {
     if(err) return cb(err + "\n" + stderr);
     var lines = stdout.split(/\r?\n/);
-    var i, m;
+    var i, m, uri;
     for(i=0; i < lines.length; i++) {
       if(m = lines[i].match(r)) {
-        if(vendor === 'brother') {
-          return installBrother(vendor, serial, cb);
-        } else {
-          return installDymo(m[0], model, serial, cb);
-        }
+        uri = m[0];
+        return cb(null, uri);
       }
     }
+    return cb(new Error("Printer with vendor '"+vendor+"' and serial '"+serial+"' not detected by CUPS. Maybe don't use the --use-usb-uri argument"));
   });
+
+}
+
+function getURI(vendor, serial, cb) {
+
+  if(argv['use-usb-uri']) {
+    return getUsbURI(vendor, serial, cb);
+  } else {
+    return getFileURI(vendor, serial, cb);
+  }
+}
+
+// vendor is "brother" or "dymo"
+function installPrinter(vendor, vendorID, model, serial, cb) {
+  vendor = vendor.toLowerCase();
+  model = model.replace(/_/g, ' ');
+
+//  console.log("VENDOR:", vendor, "||", model, "||", serial);
+  
+  if(vendor === 'brother') {
+    return installBrother(vendor, vendorID, serial, cb);
+  } else if(vendor === 'dymo') {
+    return installCups(vendor, vendorID, model, serial, cb);
+  } else {
+    return cb(new Error("Printer from unsupported vendor '"+vendor+"'"));
+  }
+}
+
+function oneLineDevice(device) {
+  if(!device.manufacturer) {
+    return 'Vendor ID: ' + device.vendorId + ', Product ID: ' + device.vendorId;
+  }
+  var str = device.manufacturer + ': ' + device.deviceName;
+  if(device.serialNumber) {
+    str += ' with serial: ' + device.serialNumber;
+  }
+  return str;
 }
 
 function isDeviceSupported(device) {
-  if(supportedDevices[device.manufacturer.toLowerCase()]) {
-
-    // TODO implement matching the model
-    return true;
+  debug("Checking if device is supported: ", oneLineDevice(device));
+  var manufacturer = supportedDevices[device.manufacturer.toLowerCase()];
+  if(!manufacturer) {
+    return false;
   }
 
-  return false;
+  if(manufacturer.indexOf(device.vendorId) < 0) {
+    return false
+  }
+  return true;
 }
 
 
@@ -187,8 +404,9 @@ function onDetectPrinter(device, cb) {
     if(cb) cb();
     return;
   }
-  console.log("GOT:", device);
-  installPrinter(device.manufacturer, device.deviceName, device.serialNumber, function(err, installed) {
+  console.log("Detected supported printer:", device);
+  
+  installPrinter(device.manufacturer, device.vendorId, device.deviceName, device.serialNumber, function(err, installed) {
     if(err) {
       console.error("Installing printer failed:", device, "\n", err);
       if(cb) cb();
@@ -205,13 +423,24 @@ function onDetectPrinter(device, cb) {
 
 function init() {
   if(process.geteuid() !== 0) {
-    console.error("You must run this program as root");
-    console.error("since it needs to modify /etc/udev/rules.d/");
-    console.error("and run `udevadm trigger` as root");
+    console.error("You must run this program as root since it needs to modify /etc/udev/rules.d/");
+    console.error("and needs to run `udevadm trigger` as root");
     usbDetect.stopMonitoring();
     process.exit(1);
   }
-
+  
+  if(argv['uninstall-all']) {
+    uninstallAll(function(err, number) {
+      if(err) {
+        console.error(e);
+        process.exit(1);
+      }
+      console.log("Uninstalled:", number, "printers");
+      process.exit(0);
+    });
+    return;
+  }
+  
   console.log("Checking for already connected printers")
 
   // detect USB devices that are already connected
@@ -231,6 +460,10 @@ function init() {
 
 }
 
-init();
+
+checkFileDevice(function(err, isFileDeviceOptionEnabled) {
+  init();
+});
+
 
 
